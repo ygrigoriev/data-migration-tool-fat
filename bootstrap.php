481918e354707e7240c39286f45b5957e46b34c8
Yurii Hryhoriev <yhryhoriev@magento.com>
2016-08-16 11:41:34 +0300

    MAGETWO-56570: Research possibility to run tests in vendor directory

--- bootstrap.php
+++ bootstrap.php
@@ -4,12 +4,12 @@
  * See COPYING.txt for license details.
  */
 
-defined('MTF_BOOT_FILE') || define('MTF_BOOT_FILE', __FILE__);
-//defined('MTF_BP') || define('MTF_BP', 'C:/Projects/magento2ce/vendor/magento/data-migration-tool-fat');
-defined('MTF_BP') || define('MTF_BP', 'C:\Projects\magento2ce\dev\tests\functional');
-defined('MTF_TESTS_PATH') || define('MTF_TESTS_PATH', 'C:/Projects/magento2ce/dev/tests/functional/tests/app/');
-defined('MTF_STATES_PATH') || define('MTF_STATES_PATH', 'C:/Projects/magento2ce/dev/tests/functional/lib/Magento/Mtf/App/State/');
-
-require_once __DIR__ . '/../../../app/bootstrap.php';
+require_once realpath(__DIR__ . '/../../../') . '/app/bootstrap.php';
 restore_error_handler();
-require_once __DIR__ . '/../../../vendor/autoload.php';
+
+defined('MTF_BOOT_FILE') || define('MTF_BOOT_FILE', __FILE__);
+defined('MTF_BP') || define('MTF_BP', BP . '/dev/tests/functional');
+defined('MTF_TESTS_PATH') || define('MTF_TESTS_PATH', BP . '/dev/tests/functional/tests/app/');
+defined('MTF_STATES_PATH') || define('MTF_STATES_PATH', BP . '/dev/tests/functional/lib/Magento/Mtf/App/State/');
+
+require_once BP . '/dev/tests/functional/vendor/autoload.php';
